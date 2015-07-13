e1512cb3eacede9b8329dd4d6478e65beed8e68a
Michal Lopuszynski <lopusz@users.noreply.github.com>
2015-07-13 15:26:43 +0200

    Importing the hunspell_stemmer project

---
+++ project.clj
@@ -0,0 +1,21 @@
+(defproject stemmer-hunspell "0.1.0"
+
+  ; GENERAL OPTIONS
+
+  :description "Hunspell stemmer functionality filtered out of Lucene 5.2.1."
+  :url "http://github.com/lopusz/stemmer-hunspell"
+  :license {:name " Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
+  ;; Options used by Java
+  ;;; run with assertions enabled
+
+  :java-source-paths ["src"]
+  :omit-source true
+  :aot :all  
+  
+  ; DEPENDENCIES
+
+  :dependencies [
+    [org.apache.lucene/lucene-core "5.2.1"]
+    [org.apache.lucene/lucene-analyzers-common "5.2.1"]
+   ])
